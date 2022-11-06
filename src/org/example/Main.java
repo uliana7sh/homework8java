caa4a2884fe42209f0b4ef49af3ed48fc43cccb2
uliana shevchuk <uliyana3@gmail.com>
2022-11-06 18:11:42 +0100

    Homework8java

---
+++ src/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[]args){
+   int maxValue=10;
+    int sum=0;
+    for(int i=0;i<=maxValue;i++){
+    sum = sum+1;}
+    System.out.println(sum);
+    System.out.println(1+2+3+4+5+6+7+8+9+10);
+    }
+}
